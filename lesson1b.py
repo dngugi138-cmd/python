c860e6aa8207d169113a8fb7cb7abffb47173fb4
dngugi138-cmd <dngugi138@gmail.com>
2026-02-04 14:14:30 +0300

    updated files

---
+++ lesson1b.py
@@ -0,0 +1,18 @@
+# python Data types
+# String - It is a series of characters that are inside of quotes
+name = "Joseph Kainembuga"
+print(name)
+print(type(name))
+
+# Numeric
+# Int - This indicate a whole number in python. For an interger, we don't use quotes
+age = 39
+print("The age of a person is: ", age)
+print(type(age))
+
+# float - This is a number with a decimal
+coordinates = 15.4524
+print('The coordinates are: ', coordinates)
+print(type(coordinates))
+
+# Research on complex data types
---
+++ lesson1d.py
@@ -0,0 +1,11 @@
+# Create a python program that is able to calculate the simple interest given principle as 50000, rate as 5% and time as 8 years. SI = (p * R * t)/100
+principle = 50000
+rate = 5
+time = 8
+simpleinterest = (principle * rate * time) / 100
+print("The simpleinterest is: ", simpleinterest)
+
+# Assignment
+# 1. Write a python program that is able to calculate the BMI of a person whose weight is 78kgs and height is 1.75 BMI = (weight) / (height squared)
+# 2. Find the Area and perimeter of a rectangle whose length is 48cm and width is 25cm
+# Research on python list, tuple and Dictionary Data types.
